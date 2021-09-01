7da2c160acb2d7c013cb06cd0de111b7b61c6bdd
Osman Solomon <39956261+OzmanSolomon@users.noreply.github.com>
2021-09-01 15:48:09 +0400

    default instance

--- Sources/OziApiManager/ApiManager.swift
+++ Sources/OziApiManager/ApiManager.swift
@@ -1,31 +1,29 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Osman Solomon on 31/08/2021.
 //
 
-
-import Foundation
 import Alamofire
+import Foundation
 
-public typealias apiSuccess = (_ data: Data?,_ errorString: String?,_ statusCode:Int?) -> ()
+public typealias apiSuccess = (_ data: Data?, _ errorString: String?, _ statusCode: Int?) -> ()
 public typealias apiFailure = (_ errorString: Error) -> ()
 
-public class ApiManager{
-    public init(){
-        
-    }
-   public func getMethod(url:String, withSuccess success: @escaping apiSuccess, withapiFiluer failure: @escaping apiFailure) {
+public struct ApiManager {
+    public static let `default` = ApiManager()
+    public init() {}
+
+    public func getMethod(url: String, withSuccess success: @escaping apiSuccess, withapiFiluer failure: @escaping apiFailure) {
         AF.request(url)
-            .responseData() { response in
+            .responseData { response in
                 switch response.result {
                 case .success(let value):
                     success(value, response.error?.localizedDescription, response.response?.statusCode)
-                 case .failure(let error):
+                case .failure(let error):
                     failure(error)
                 }
             }
-      }
-    
-}
+    }
+} 
---
+++ Sources/OziApiManager/OziApiManager.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Osman Solomon on 01/09/2021.
+//
+
+ public let OziApi = ApiManager.default
